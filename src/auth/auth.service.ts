f254866c2025bfce5f12c30dea65fb0e510918f7
mayuresh-byte <mayureshpitambare@gmail.com>
2024-07-13 12:27:53 +0530

    Feat: SignIn logic with JWT

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,10 +3,12 @@ import { PrismaService } from "src/prisma/prisma.service";
 import { AuthDto } from "./dto";
 import * as argon from 'argon2';
 import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
+import { JwtService } from "@nestjs/jwt";
+import { ConfigService } from "@nestjs/config";
 
 @Injectable()
 export class AuthService {
-    constructor(private prisma: PrismaService) {}
+    constructor(private prisma: PrismaService, private jwt: JwtService, private config: ConfigService) {}
 
     async signUp(dto: AuthDto) {
         try {
@@ -18,8 +20,8 @@ export class AuthService {
                 }
             })
             // const { email, createdAt} = user;
-            delete user.hash;
-            return {newUser: user}
+            // delete user.hash;
+            return this.signToken(user.id, user.email);
         } catch (error) {
             if(error instanceof PrismaClientKnownRequestError) {
                 if(error.code === 'P2002') {
@@ -30,7 +32,38 @@ export class AuthService {
         }
     }
 
-    signIn() {
-        return {msg: 'I have singned in'}
+    async signIn(dto: AuthDto) {
+        const user = await this.prisma.user.findUnique({
+            where: {
+                email: dto.email
+            }
+        });
+        if (!user) {
+            throw new ForbiddenException('Credentials incorrect !');
+        }
+
+        const passwordMatches = await argon.verify(user.hash, dto.password);
+        if (!passwordMatches) {
+            throw new ForbiddenException('Credentials incorrect !');
+        }
+
+        return this.signToken(user.id, user.email);
+    }
+
+    async signToken(userId: number, email: string): Promise<{}> {
+        console.log(this.config.get('JWT_SECRET'));
+        const payload = {
+            sub: userId, // Generally sub is used to represnt uniqueId
+            email: email
+        };
+
+        const token = await this.jwt.signAsync(payload, {
+            expiresIn: '15m',
+            secret: this.config.get('JWT_SECRET')
+        });
+
+        return {
+            access_token: token
+        };
     }
 }
