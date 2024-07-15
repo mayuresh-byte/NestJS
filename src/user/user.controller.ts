06d44bbf3c6434479728a86244ab074e1e373dd8
mayuresh-byte <mayureshpitambare@gmail.com>
2024-07-15 19:57:26 +0530

    Feat: edit user route

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,23 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Param, Put, Req, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { Request } from 'express';
+import { EditUserDto } from './dto';
+import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
+    constructor(private userService: UserService) {}
     @UseGuards(AuthGuard('jwt'))
     @Get('me')
     getMe(@Req() req: Request) {
-        return "user info";
+        return req.user;
+    }
+
+    @UseGuards(AuthGuard('jwt'))
+    @Put(':id')
+    editUser(@Param('id') userId: string, @Body() dto: EditUserDto) {
+        // console.log(req.user);
+        const parseduserId = parseInt(userId, 10);
+        return this.userService.editUser(parseduserId, dto);
     }
 }
